3306641278f985a0871dd2530413279155836862
josh.salomon@gmail.com <josh.salomon@gmail.com>
2020-11-15 11:56:21 +0200

    inital version cont - split some reusable functions into file crush-utils.sh

--- check-pool-affinity.sh
+++ check-pool-affinity.sh
@@ -10,18 +10,7 @@
 # extra development effort.
 #####
 
-#
-# The following 4 arrays represent a single array with structure elements that we use for holding
-# crush bucket information (parent, node name, node type, node index) - this is enough information
-# for testing later whether a pool has read affinity (in addition to the type of the failure domain
-# level).
-#
-declare -A crush_parents
-declare -A crush_node_type
-declare -A crush_node_name
-declare -A node_indices
-
-failure_domain_type=""
+. $(dirname "$0")/crush-utils.sh
 
 function usage() {
     #
@@ -34,31 +23,6 @@ function usage() {
     exit 1
 }
 
-function find_failure_domain()
-{
-    # 
-    #TODO - the first level in the tree which has more than one node. (so root/region/AZ works 
-    # as well as root/AZ or root/datacenter)
-    echo "" > /dev/null
-    local node_idx=0
-    while true; do
-        local node_info=$(echo $1 | jq .nodes[$node_idx])
-        local n_children=$(echo $node_info | jq ".children | length")
-        echo "node index $node_idx, nChildren $n_children"
-        first_child=$(echo $1 | jq .nodes[$node_idx].children[0])
-        if [ $n_children -gt 1 ]; then
-            echo "node $node_id - below is failure domain, such as $first_child"
-            failure_domain_type=${crush_node_type[$first_child]}
-            echo "Failure domain type is $failure_domain_type"
-            break
-        elif [ $n_children -eq 1 ]; then
-            node_idx=${node_indices[$first_child]}
-        else 
-            echo "*ERROR*: Did not find a failure domain in tree. Is this a production-like system?"
-            exit 0
-        fi
-    done
-}
 
 function has_read_affinity() {
     echo "" > /dev/null
@@ -87,36 +51,6 @@ function has_read_affinity() {
     ##echo " == Iterated over $prim_count PGs"
 }
 
-function build_crush_tree() {
-    ## 
-    # This function builds the crush tree information in the 3 arrays
-    # crush_parents, crush_node_type and crush_node_name
-    ##
-    local num_nodes=$(echo $1 | jq ".nodes | length")
-##    echo " found $num_nodes nodes in the crush tree"
-
-    for  (( i = 0 ; i < $num_nodes ; i++ ))
-    do
-        local node_info=$(echo $1 | jq .nodes[$i])
-##        echo "====="
-##        echo $node_info
-##        echo "-----"
-        local n_children=$(echo $node_info | jq ".children | length")
-        local id=$(echo $node_info | jq .id)
-        local type=$(echo $node_info | jq .type | sed 's/"//g')
-        local name=$(echo $node_info | jq .name | sed 's/"//g')
-        echo "node $id: name is $name, type is $type, index is $i"
-        crush_node_name[$id]=$name
-        crush_node_type[$id]=$type
-        node_indices[$id]=$i
-        for (( child = 0 ; child < $n_children ; child++ ))
-        do
-            local ch_id=$(echo $node_info | jq .children[$child])
- ##           echo "Parent of $ch_id is $id"  
-            crush_parents[$ch_id]=$id
-        done
-    done    
-}
 
 if [ -z "$1" ]; then
     usage
@@ -148,14 +82,14 @@ else
 fi
 
 build_crush_tree $crush_tree_json
-find_failure_domain $crush_tree_json
+find_failure_domains $crush_tree_json
 
 echo "Failure domain type is $failure_domain_type"
 
 ##debug
-for i in "${!crush_node_name[@]}"; do
-    echo "Node $i, name ${crush_node_name[$i]}, type ${crush_node_type[$i]}, parent ${crush_parents[$i]}" 
-done
+##for i in "${!crush_node_name[@]}"; do
+##    echo "Node $i, name ${crush_node_name[$i]}, type ${crush_node_type[$i]}, parent ${crush_parents[$i]}" 
+##done
 
 pool_num=$(ceph osd pool stats | awk -v PN="$pool_name" '{ if ($1 == "pool" && $2 == PN) {print $4}}')
 
@@ -172,8 +106,8 @@ primaries=$(ceph pg dump pgs_brief 2>/dev/null | grep "^$pool_num." | awk '{ spl
 # line does not work well when piped to the first line 
 #
 primaries=$(echo $primaries | sed "s/ /\n/g" | sort | uniq )
-primaries=$( echo $primaries | sed "s/ /:/g")
-echo "short primaries list: $primaries"
+primaries=$(echo $primaries | sed "s/ /:/g")
+##echo "short primaries list: $primaries"
 
 has_read_affinity
 
